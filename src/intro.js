0d339a8258ce282801e0e0c48a172eaa066c126c
Alan Richey <alan.richey@fire-engine-red.com>
2017-07-11 10:36:15 -0500

    Adding logic to preserve JQuery/moment scoping for Browserify require import

--- src/intro.js
+++ src/intro.js
@@ -8,7 +8,15 @@
 	if (typeof define === 'function' && define.amd) {
 		define([ 'jquery', 'moment' ], factory);
 	}
-	else if (typeof exports === 'object') { // Node/CommonJS
+	/**
+	 * Adding the check typeof jQuery !== 'function' so that the factory uses
+	 * the global jQuery and moment instead of its own dependencies.
+	 * Browserify will not resolve the require('jquery') to use the global
+	 * jQuery if you are using an older version of jQuery on the global scope
+	 * which happens when you do things like use jQuery 2.2.4 to run jQuery UI.
+	 * Whew.
+	 */
+	else if (typeof jQuery !== 'function' && typeof exports === 'object') { // Node/CommonJS
 		module.exports = factory(require('jquery'), require('moment'));
 	}
 	else {
